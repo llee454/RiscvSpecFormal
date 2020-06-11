4f73f27a433f1f64b58662756c267cc26c964fca
Ubuntu <ubuntu@ip-172-31-33-80.us-east-2.compute.internal>
2020-06-11 19:30:26 +0000

    Fixed Target.hs so that it can generate Verilog using the new Fin definition.

--- Haskell/Target.hs
+++ Haskell/Target.hs
@@ -1,6 +1,6 @@
-module Target (module Syntax, module Rtl, module Word, module Fin, module EclecticLib, module PeanoNat, module CompilerSimple, module Extraction, module LibStruct, module Instance, module Test, getRtl, rtlMod) where
+module Target (module Syntax, module Rtl, module Word, module Fin, {- module EclecticLib, -} module PeanoNat, module CompilerSimple, module Extraction, module LibStruct, module Instance, module Test, getRtl, rtlMod) where
 import CompilerSimple hiding (unsafeCoerce, __)
-import EclecticLib hiding (__)
+-- import EclecticLib hiding (__)
 import PeanoNat
 import Fin hiding (unsafeCoerce, Any, f2n)
 import Instance hiding (unsafeCoerce, __)
